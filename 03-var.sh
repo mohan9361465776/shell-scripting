141ddd862122e6cab477c1c6d9a544c78f531474
mohan <mohangurrampati321@gmail.com>
2022-07-10 10:13:05 +0530

    Third commit

---
+++ 03-var.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+## A variable is something which holds the values dynamically
+a=10
+#10 is a string here
+b=abc
+#abc is also a string
+#when to use double ??? when your input us having any special characters,then u need to close in quotes
+echo $a # $a will print the value os a
+echo $b
