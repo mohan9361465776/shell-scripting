3951be54c8b7cbf0038754cd53472d1680c95b46
mohan <mohangurrampati321@gmail.com>
2022-07-10 12:23:10 +0530

    Third commit

--- 05-special_variable.sh
+++ 05-special_variable.sh
@@ -2,3 +2,10 @@
 # $0 : This will print the script name that you running
 # $? : This will show you the status code of last execution
 echo "script we are executing is $0"
+# Special variables
+# $0 to $n , $* , $@ , $$
+# Through command line you can pass up to 9 variables
+echo "The value of x is $1"
+echo "Name of trainer is $2"
+# sh script.sh 10 20 30
+#              $1 $2 $3
