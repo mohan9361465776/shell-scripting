1aae51399f52be157209bf436cae826fd47636d0
mohan <mohangurrampati321@gmail.com>
2022-07-10 09:56:43 +0530

    Third commit

--- 02-print.sh
+++ 02-print.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-#echo -e "\n[COL-CODEm MESSAGE \e[0m"
+#echo -e "\e[COL-CODEm MESSAGE \e[0m"
 echo -e "\e[32m i am printing Green COLOR \e[0m"
 echo -e "\e[31m i am printing Red COLOR \e[0m"
 echo -e "\e[33m i am printing Yellow COLOR \e[0m"
 echo -e "\e[34m i am printing Blue COLOR \e[0m"
 echo -e "\e[36m i am printing Cyan COLOR \e[0m"
+#To have a background Color
+#echo -e "\e[background; COL-CODEm MESSAGE \e[0m"
+echo -e "\e[45;33m i am printing Colors \e[0m"
