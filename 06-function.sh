af1cddf73a3cbb1ef0af42525c79b995a360c547
mohan <mohangurrampati321@gmail.com>
2022-07-10 12:51:57 +0530

    Third commit

--- 06-function.sh
+++ 06-function.sh
@@ -11,7 +11,7 @@ echo "I am the first line in the function";
 echo "Today date is $(date +%\F)";
 echo "Number of opened sessions are $(who | wc -l)";
 echo "Calling SAM function"
-SAM
+sam
 echo "Function f completed ....BYE"
 }
 echo "showing you how to call a function"
