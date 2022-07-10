54c93caeeaba730006d6ad06ca36b219660bcfc7
mohan <mohangurrampati321@gmail.com>
2022-07-10 12:43:43 +0530

    Third commit

--- 06-function.sh
+++ 06-function.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #Functions help you in avoiding the same set of code multiple times.
 #Declare it is a function and call it whenver or wherever you want.
-f()
+fa()
 {
 echo "I am the first line in the function";
 echo "Today date is $(date+%\F)";
 echo "Number of opened sessions are $(who | wc -l)";
 }
 echo "showing you how to call a function"
-F
+fa
 sleep 5
-F
+fa
 sleep 5
-F
+fa
