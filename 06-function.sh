11f4b268cfc38f8053b0a1bdbe609184918ab6e4
mohan <mohangurrampati321@gmail.com>
2022-07-10 12:50:10 +0530

    Third commit

--- 06-function.sh
+++ 06-function.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 #Functions help you in avoiding the same set of code multiple times.
 #Declare it is a function and call it whenver or wherever you want.
+sam() {
+    echo "I am a command from SAM"
+
+}
 fa()
 {
 echo "I am the first line in the function";
 echo "Today date is $(date +%\F)";
 echo "Number of opened sessions are $(who | wc -l)";
+echo "Calling SAM function"
 echo "Function f completed ....BYE"
 }
 echo "showing you how to call a function"
